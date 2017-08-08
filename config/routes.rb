5cbffc595c79afe72661450223012f99c898ac43
Lucas Andrade <lucasandradeunb@gmail.com>
2017-08-08 14:51:49 -0300

    create navegation and navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
-  get 'pages/about' => 'pages#about'
+  root 'pages#home', as: 'home'
+  get 'pages/about' => 'pages#about', as: 'about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
