ecbb01c2ec6145e849ccad26ae6f0fbdc0cb9599
Lucas Andrade <lucasandradeunb@gmail.com>
2017-08-12 12:35:10 -0300

    Add logic for posts CRUD only with login users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :check_log_in, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -10,7 +11,6 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find_by(url: params[:url])
   end
 
   # GET /posts/new
@@ -63,9 +63,16 @@ class PostsController < ApplicationController
   end
 
   private
+    def check_log_in
+      if logged_in? == false
+        redirect_to home_path
+      end
+    end
+
+
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Post.find_by(url: params[:url])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
