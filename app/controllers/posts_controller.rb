e0204185a1faa4127ecedc0507ff0079ccafe682
Lucas Andrade <lucasandradeunb@gmail.com>
2017-08-15 14:26:37 -0300

    generate comments CRUD and add comments to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,8 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post_comments = Comment.where(post_id: @post.id)
+    @comment = Comment.new
   end
 
   # GET /posts/new
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
 
   has_one :category
   has_one :subcategory
+  has_many :comments
 
   def to_param
     url
