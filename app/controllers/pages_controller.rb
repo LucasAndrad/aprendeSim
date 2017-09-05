3636edda83a140be14ad569c465a57ed572625b5
Lucas Andrade <lucasandradeunb@gmail.com>
2017-09-04 23:25:09 -0300

    Render posts_all partial on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   
   def home
-
+    @posts = Post.all.order("created_at DESC").limit(10)
+    @categories = Category.all.order(:name).limit(10)
+    @posts_most_views = Post.all.order("visits DESC").limit(15)
   end
 
   def about
