3a681e3ce273dba9ec4daa1e00ced2aee299ab66
Lucas Andrade <lucasandradeunb@gmail.com>
2017-09-24 13:30:54 -0300

    Add name validation for category and subcategory

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,9 @@ class Category < ApplicationRecord
 
   has_many :subcategories
   has_many :posts
+
+  validates :name, 
+    presence: {message: 'O nome é obrigatório'},
+    uniqueness: {message: 'Já exite uma Categoria com esse nome'}
   
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -3,4 +3,8 @@ class Subcategory < ApplicationRecord
   belongs_to :category
   has_many :posts
 
+  validates :name, 
+    presence: {message: 'O nome é obrigatório'},
+    uniqueness: {message: 'Já exite uma Categoria com esse nome'}
+
 end
